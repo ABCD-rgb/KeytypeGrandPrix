2905da816252896c2ebafbd390038472a52116da
Christian Pascua <cmpascua@up.edu.ph>
2024-04-21 16:58:35 +0800

    feat: setup welcome screen graphics

--- src/typingGame/Game.java
+++ src/typingGame/Game.java
@@ -11,6 +11,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -26,14 +27,20 @@ public class Game {
 	private Group root;
 	private Canvas canvas;
 	private DropShadow shadow;	// shadow effect when button is clicked
+	private Button b1;
+    private Button b2;
+    private Button b3;
 	
 	// constants
-	public final static int WINDOW_WIDTH = 800;	// same as height
+	public final static int WINDOW_WIDTH = 800;
+	public final static int WINDOW_HEIGHT = 600;
 	public final static int WINDOW_CENTER = WINDOW_WIDTH / 2;
-	public final static Image BG_IMG = new Image("images/concrete-floor.jpg", Game.WINDOW_WIDTH, Game.WINDOW_WIDTH, false, false);
+	public final static Image BG_IMG = new Image("images/welcome_bg.png", Game.WINDOW_WIDTH, Game.WINDOW_WIDTH, false, false);
+	public final static ImageView LOGO = new ImageView(new Image("images/logo.png"));
+	public final static Image WINDOWLOGO = new Image("images/window_logo.png");
 
 	public Game() {
-		this.canvas = new Canvas(Game.WINDOW_WIDTH, Game.WINDOW_WIDTH);
+		this.canvas = new Canvas(Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);
 		this.root = new Group();
 		// canvas is showing at the root
 		this.root.getChildren().add(this.canvas);
@@ -45,7 +52,10 @@ public class Game {
 	// setting up stage and running the app
 	public void setStage(Stage stage) {
 		this.stage = stage;
-		stage.setTitle("KeyTypeGrandPrix");
+		stage.setTitle("Key Type Grand Prix - Typing Game");
+		
+		// set the window logo
+	    stage.getIcons().add(WINDOWLOGO);
 		
 		// game entry point
 		this.initMenu(stage);
@@ -56,7 +66,7 @@ public class Game {
 	
 	// display the main menu
 	private void initMenu(Stage stage) {
-		Canvas canvas = new Canvas(Game.WINDOW_WIDTH, Game.WINDOW_WIDTH);
+		Canvas canvas = new Canvas(Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		
 		// Image bg = new Image("images/concrete-floor.png", Game.WINDOW_WIDTH, Game.WINDOW_WIDTH, false, false);
@@ -77,51 +87,76 @@ public class Game {
 	private VBox createMenuButtons() {
 		// NOTE: VBox is used to layout its children in a single vertical column
 		VBox vbox = new VBox();
-		vbox.setAlignment(Pos.CENTER);
-		vbox.setPadding(new Insets(5));
-		vbox.setSpacing(10);
+		vbox.setAlignment(Pos.BASELINE_CENTER);
+		vbox.setPadding(new Insets(50, 5, 5, 5));
+		vbox.setSpacing(20);
+		
+		LOGO.setFitWidth(600);
+	    LOGO.setFitHeight(200);
 
-		Button b1 = new Button("New Game");
-		Button b2 = new Button("Instructions");
-		Button b3 = new Button("About");
+	    b1 = new Button("New Game");
+        b2 = new Button("Instructions");
+        b3 = new Button("About");
 
-		Font font = Font.font("Times New Roman", FontWeight.BOLD, 35); // yung 30 here is yung size kaya malaki yung button sa display
+		Font font = Font.font("Verdana", FontWeight.BOLD, 25); // yung 30 here is yung size kaya malaki yung button sa display
 		b1.setFont(font);
 		b2.setFont(font);
 		b3.setFont(font);
-		b1.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #9F2B00; -fx-background-color: linear-gradient(#FF8B00, #F4D03F, #FF8B00); -fx-border-color:#9F2B00; -fx-border-radius: 15px; -fx-border-width: 5px" );
-		b2.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #9F2B00; -fx-background-color: linear-gradient(#FF8B00, #F4D03F, #FF8B00); -fx-border-color:#9F2B00; -fx-border-radius: 15px; -fx-border-width: 5px" );
-		b3.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #9F2B00; -fx-background-color: linear-gradient(#FF8B00, #F4D03F, #FF8B00); -fx-border-color:#9F2B00; -fx-border-radius: 15px; -fx-border-width: 5px" );
+		b1.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #314528; -fx-background-color: #9BE86B; -fx-border-color:#314528; -fx-border-radius: 15px; -fx-border-width: 5px" );
+		b2.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #343857; -fx-background-color: #A2D9FF; -fx-border-color:#343857; -fx-border-radius: 15px; -fx-border-width: 5px" );
+		b3.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #343857; -fx-background-color: #A2D9FF; -fx-border-color:#343857; -fx-border-radius: 15px; -fx-border-width: 5px" );
 
-		vbox.getChildren().addAll(b1,b2,b3);//add the buttons to the vbox
-
-		// actions when a specific button is pressed
-		b1.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				b1.setEffect(shadow);
-				changeScene(stage, "game");
-			}
-		});
-		b2.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				b2.setEffect(shadow);
-				changeScene(stage, "instructions");
-			}
-		});
-		b3.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				b3.setEffect(shadow);
-				changeScene(stage, "about");
-			}
-		});
+		vbox.getChildren().addAll(LOGO,b1,b2,b3);//add the logo & buttons to the vbox
 		
-		return vbox;
+		// actions when a specific button is pressed
+		b1.setOnMousePressed(event -> darkenButton(b1));
+        b1.setOnMouseReleased(event -> resetButtonStyle(b1));
+        b1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                b1.setEffect(shadow);
+                changeScene(stage, "game");
+            }
+        });
+
+        b2.setOnMousePressed(event -> darkenButton(b2));
+        b2.setOnMouseReleased(event -> resetButtonStyle(b2));
+        b2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                b2.setEffect(shadow);
+                changeScene(stage, "instructions");
+            }
+        });
+
+        b3.setOnMousePressed(event -> darkenButton(b3));
+        b3.setOnMouseReleased(event -> resetButtonStyle(b3));
+        b3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                b3.setEffect(shadow);
+                changeScene(stage, "about");
+            }
+        });
+
+        return vbox;
 	}
 	
+	private void darkenButton(Button button) {
+		if (button == b1) {
+			button.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #FFFFFF; -fx-background-color: #6B8E23; -fx-border-color:#314528; -fx-border-radius: 15px; -fx-border-width: 5px");
+		} else {
+			button.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #FFFFFF; -fx-background-color: #6EA5CB; -fx-border-color:#343857; -fx-border-radius: 15px; -fx-border-width: 5px");
+		}
+    }
 	
+	private void resetButtonStyle(Button button) {
+	    if (button == b1) {
+	        button.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #314528; -fx-background-color: #9BE86B; -fx-border-color:#314528; -fx-border-radius: 15px; -fx-border-width: 5px");
+	    } else {
+	        button.styleProperty().setValue("-fx-background-radius: 15px; -fx-text-fill: #343857; -fx-background-color: #A2D9FF; -fx-border-color:#343857; -fx-border-radius: 15px; -fx-border-width: 5px");
+	    }
+	}
 	
 	// scene picker
 	protected void changeScene(Stage stage, String string) {
